1adf6d63b5ca6479056d3861cd40819c98db6f71
Felipe Kobra <139709626+FelipeKobra@users.noreply.github.com>
2025-02-13 22:46:51 -0300

    Add files via upload

---
+++ next_release_version.ps1
@@ -0,0 +1,44 @@
+param (
+    [switch]$dev
+)
+
+$actual_version_with_prefix = git describe --tags --abbrev=0
+$actual_version = $actual_version_with_prefix -replace '^v', ''
+$major, $minor, $patch = $actual_version -split '\.'
+$major = [int]$major
+$minor = [int]$minor
+$patch = [int]$patch
+
+$range = "$actual_version_with_prefix..@"
+$commits = git log $range --reverse --format=%s%b
+
+foreach ($commit in $commits)
+{
+    if ($commit -cmatch "BREAKING CHANGE|^\w+!")
+    {
+        if ($dev)
+        {
+            $minor += 1
+            $patch = 0
+        }
+        else
+        {
+            $major += 1
+            $minor = 0
+            $patch = 0
+        }
+    }
+    elseif ($commit -match "^feat")
+    {
+        $minor += 1
+        $patch = 0
+    }
+    elseif ($commit -match "^fix")
+    {
+        $patch += 1
+    }
+}
+
+$next_version = "$major.$minor.$patch"
+
+Write-Host "Next release version: $next_version"
